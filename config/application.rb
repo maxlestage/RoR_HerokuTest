4277886635a00b40c4c5ea3d4c2e76148459db16
Max Lestage <maxlestage@icloud.com>
2020-10-12 17:48:28 +0200

    +2

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.time_zone = 'Europe/Paris'
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
